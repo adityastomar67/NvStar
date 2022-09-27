abe3483a7c0ae55df07ca9bfac19dc03503b5fef
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-09-27 09:03:02 +0530

    added plugings

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -37,6 +37,40 @@ return packer.startup(function(use)
     use("wbthomason/packer.nvim") -- Have packer manage itself
     use("folke/which-key.nvim")
 
+    use({"akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons"})
+    use({"lewis6991/impatient.nvim", config = "require('impatient')"})
+    use("nvim-treesitter/nvim-treesitter")
+    use("tamton-aquib/staline.nvim")
+    use("kyazdani42/nvim-tree.lua")
+    use({
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup({}) end,
+        after = "nvim-cmp"
+    })
+    use("norcalli/nvim-colorizer.lua")
+    use("xiyaowong/telescope-emoji.nvim")
+    use("nvim-telescope/telescope-media-files.nvim")
+    use("nvim-lua/plenary.nvim")
+    use("nvim-telescope/telescope.nvim")
+    use("goolord/alpha-nvim")
+    use("neovim/nvim-lspconfig")
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-vsnip")
+    use("hrsh7th/vim-vsnip")
+    use("onsails/lspkind-nvim")
+    use("elkowar/yuck.vim")
+    use("dstein64/vim-startuptime")
+    use("nathom/filetype.nvim")
+    use("kyazdani42/nvim-web-devicons")
+    use("williamboman/nvim-lsp-installer")
+    use("akinsho/toggleterm.nvim")
+    use("rcarriga/nvim-notify")
+    use("kg8m/vim-simple-align")
+    use("RishabhRD/nvim-cheat.sh")
+
     -- Automatically set up your configuration after cloning packer.nvim
     if PACKER_BOOTSTRAP then require("packer").sync() end -- Put this at the end after all plugins
 end)

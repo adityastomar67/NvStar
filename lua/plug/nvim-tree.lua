bdac6537601b76bccb5aab4014994081a62b5778
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-09-29 21:57:33 +0530

    tree size changing

--- lua/plug/nvim-tree.lua
+++ lua/plug/nvim-tree.lua
@@ -1,6 +1,35 @@
+local winsize = require("core.utils").winsize
+local mode = require("core.constants").modes
+local map = require("core.utils").map
+local nvim_tree_events = require("nvim-tree.events")
+
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then return end
+
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then return end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
 vim.g.nvim_tree_width = 25
-require("nvim-tree").setup({
+
+local DEFAULT_WIDTH = 25
+
+local function inc_width()
+    local width, _height = winsize()
+    local new_width = width + 10
+    vim.cmd(string.format("NvimTreeResize %d", new_width))
+end
+
+local function dec_width()
+    local width, _height = winsize()
+    local new_width = width - 10
+    if new_width < DEFAULT_WIDTH then return end
+    vim.cmd(string.format("NvimTreeResize %d", new_width))
+end
+
+local opts = {
     renderer = {
+        highlight_opened_files = "name",
         indent_markers = {enable = true},
         icons = {
             glyphs = {
@@ -41,19 +70,36 @@ require("nvim-tree").setup({
     hijack_directories = {enable = true},
     view = {
         mappings = {
+            custom_only = false,
             list = {
-                {key = "l", action = "edit"}, {key = "o", action = "edit"},
-                {key = "<cr>", action = "edit"},
+                {key = {"l", "<CR>", "o"}, cb = tree_cb("edit")},
+                {key = "h", cb = tree_cb("close_node")},
+                {key = "<C-v>", action = "vsplit"},
+                {key = "<C-x>", action = "split"},
+                {key = "a", action = "create"},
                 {key = "I", action = "toggle_ignored"},
                 {key = "H", action = "toggle_dotfiles"},
                 {key = "R", action = "refresh"},
                 {key = "=", action = "preview"},
-                {key = "X", action = "xdg_open", action_cb = xdg_open}
+                {key = "X", action = "xdg_open", action_cb = xdg_open},
+                {key = "d", action = "remove"}, 
+                {key = "r", action = "rename"},
+                {key = "y", action = "copy_name"},
+                {key = "Y", action = "copy_path"},
+                {key = "gy", action = "copy_absolute_path"},
+                {key = "q", action = "close"},
+                {key = "W", action = "collapse_all"},
+                {key = "S", action = "search_node"},
+                {key = "<C-k>", action = "toggle_file_info"},
+                {key = ">", action = "more_wide", action_cb = inc_width},
+                {key = "<", action = "less_wide", action_cb = dec_width}
             }
         }
     },
     open_on_setup = true
-})
+}
+
+nvim_tree.setup(opts)
 
 vim.cmd([[
 autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif

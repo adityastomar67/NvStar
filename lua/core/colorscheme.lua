d426c35dfab226a4b287c093bb80cde5177a8ac5
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-09-29 21:58:02 +0530

    kanagawa setup

---
+++ lua/core/colorscheme.lua
@@ -0,0 +1,28 @@
+local cmp_status_ok, kanagawa = pcall(require, "kanagawa")
+if not cmp_status_ok then
+    return
+end
+
+local kanagawa_opts = {
+	undercurl            = true,              -- undercurl is enabled by default
+	specialReturn        = true,              -- special highlight for the return keyword
+	specialKeyword       = true,              -- special highlight for the keyword "return"
+	specialException     = true,              -- special highlight for exception handling keywords
+	dimInactive          = false,             -- dim inactive window `:h hl-NormalNC`
+	globalStatus         = false,             -- adjust window separators highlight for laststatus = 3
+	commentStyle         = { italic = true }, -- Italicize comments
+	functionStyle        = { italic = true }, -- Italicize functions
+	keywordStyle         = { italic = true }, -- Italic Keywords
+	statementStyle       = { bold   = true }, -- Bold is the default for statements.
+	typeStyle            = { italic = true }, -- Italic is used for type names.
+	variablebuiltinStyle = { italic = true }, -- Italic for builtin variables
+	colors               = {},
+	overrides            = {},
+}
+
+-- -- Data for limelight_conceal
+vim.cmd("let g:limelight_conceal_ctermfg = 'gray'")
+vim.cmd("let g:limelight_conceal_ctermfg = 240")
+vim.cmd("let g:limelight_conceal_guifg = 'DarkGray'")
+vim.cmd("let g:limelight_conceal_guifg = '#777777'")
+vim.cmd("let g:limelight_default_coefficient = 0.9")

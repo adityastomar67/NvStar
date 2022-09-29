1633cf65ac95a8435afbe12e7c683229cd478fe9
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-09-29 14:30:20 +0530

    Added bufferline

---
+++ lua/plug/bufferline.lua
@@ -0,0 +1,29 @@
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then return end
+
+local opts = {
+    options = {
+        buffer_close_icon = '',
+        modified_icon = '',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        separator_style = "thick",
+        color_icons = true,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "center",
+                separator = false
+            }
+        }
+    }
+}
+
+bufferline.setup(opts)
+
+vim.cmd [[
+  nnoremap <silent><TAB> :BufferLineCycleNext<CR>
+  nnoremap <silent><S-TAB> :BufferLineCyclePrev<CR>
+  ]]

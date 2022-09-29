acaeda1f655c6251e5d13a9f02d5f4fb4456eb6c
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-09-29 21:56:49 +0530

    additions

--- init.lua
+++ init.lua
@@ -1,31 +1,46 @@
 local impatient_ok, impatient = pcall(require, "impatient")
 if impatient_ok then
-  impatient.enable_profile()
+	impatient.enable_profile()
+end
+
+local status_ok, notify = pcall(require, "notify")
+if not status_ok then
+	return
 end
 
 for _, source in ipairs({
-  
-  -- CORE
-  "core.plugins",
-  "core.maps",
-  "core.options",  
-  "core.utils",
-  "core.autocmds",
 
-  -- PLUGS
-  "plug.whichkey",
-  "plug.nvim-tree",
-  "plug.ts-context",
-  "plug.terminal",
-  "plug.telescope",  
-  "plug.mason",
-  "plug.cmp",
-  "plug.lsp",
-  "plug.lsp-installer",
-  
+	-- CORE
+	"core.plugins",
+	"core.maps",
+	"core.options",
+	"core.utils",
+	"core.autocmds",
+	"core.colorscheme",
+
+	-- PLUGS
+	"plug.notify",
+	"plug.dashboard",
+	"plug.whichkey",
+	"plug.nvim-tree",
+	"plug.ts-context",
+	"plug.terminal",
+	"plug.telescope",
+	"plug.mason",
+	"plug.cmp",
+	"plug.lsp",
+	"plug.lsp-installer",
+	"plug.git-sign",
+	"plug.bufferline",
+	"plug.statusline",
+	"plug.null-ls",
+	"plug.todo-comments",
+
 }) do
-local status_ok, fault = pcall(require, source)
-if not status_ok then
-  vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
-end
+	local status_ok, fault = pcall(require, source)
+	if not status_ok then
+		local err = "Failed to load " .. source
+		-- vim.api.nvim_err_writeln(err  .. "\n\n" .. fault)
+		notify(err, "error")
+	end
 end

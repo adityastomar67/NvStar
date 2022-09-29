2ae20669b369718b29dc7a505321ea87d4fcd460
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-09-29 14:30:42 +0530

    notification daemon

---
+++ lua/plug/notify.lua
@@ -0,0 +1,18 @@
+local status_ok, notify = pcall(require, "notify")
+if not status_ok then return end
+
+local opts = {
+    icons = {
+        DEBUG = "  ",
+        ERROR = "  ",
+        WARN = "  ",
+        TRACE = "  ",
+        INFO = "  "
+    },
+    fps = 60,
+    max_height = 5,
+    max_widht = 10,
+    render = "minimal"
+}
+
+notify.setup(opts)

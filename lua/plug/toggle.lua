565c147de59b766eccaac088412dec853e8da528
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-10-14 20:20:13 +0530

    more toggle options

--- lua/plug/toggle.lua
+++ lua/plug/toggle.lua
@@ -1,28 +1,35 @@
 local t = vim.tbl_add_reverse_lookup({
-    ['true'] = 'false',    
-    ['True'] = 'False',
-    ['yes']  = 'no',
-    ['on']   = 'off',
-    ['left'] = 'right',
-    ['up']   = 'down',
-  })
-  
-  local setup = function(u_tbl)
-    t = vim.tbl_extend('force', t, vim.tbl_add_reverse_lookup(u_tbl or {}))
-  end
-  
-  local c = {
-    ['n'] = 'norm! ciw',
-    ['v'] = 'norm! c',
-  }
-  
-  local toggle = function()
-    local i = vim.tbl_get(t, vim.fn.expand('<cword>'))
-    xpcall(function()
-      vim.cmd(vim.tbl_get(c, vim.api.nvim_get_mode().mode) .. i)
-    end, function()
-      print('toggler: unsupported value.')
-    end)
-  end
+	
+	["true"] = "false",
+	["yes"]  = "no",
+	["on"]   = "off",
+	["left"] = "right",
+	["up"]   = "down",
+	["1"]    = "0",
 
-  return { setup = setup, toggle = toggle }
+	["True"] = "False",
+	["Yes"]  = "No",
+	["On"]   = "Off",
+	["Left"] = "Right",
+    ["Up"]   = "Down",
+})
+
+local setup = function(u_tbl)
+	t = vim.tbl_extend("force", t, vim.tbl_add_reverse_lookup(u_tbl or {}))
+end
+
+local c = {
+	["n"] = "norm! ciw",
+	["v"] = "norm! c",
+}
+
+local toggle = function()
+	local i = vim.tbl_get(t, vim.fn.expand("<cword>"))
+	xpcall(function()
+		vim.cmd(vim.tbl_get(c, vim.api.nvim_get_mode().mode) .. i)
+	end, function()
+		print("toggler: unsupported value.")
+	end)
+end
+
+return { setup = setup, toggle = toggle }

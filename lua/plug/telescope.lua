60a20539ded9b21c548efba4a63f34d01d9cb725
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-10-15 20:35:58 +0530

    updated symbol of search

--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -24,7 +24,7 @@ local SETTINGS = {
 			"--column",
 			"--smart-case",
 		},
-		prompt_prefix      = "   ",
+		prompt_prefix      = "  ",
 		selection_caret    = "  ",
 		entry_prefix       = "  ",
 		initial_mode       = "insert",

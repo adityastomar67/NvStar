4ea0b6bfb785d49ab7b508f734a1b1d84eabc98c
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-10-17 11:46:21 +0530

    cmp completion structure

--- lua/plug/cmp.lua
+++ lua/plug/cmp.lua
@@ -1,123 +1,181 @@
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-    return
+	return
 end
 
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
-    return
+	return
 end
+
+local snip_status_ok, tabnine = pcall(require, "cmp_tabnine.config")
+if not snip_status_ok then
+	return
+end
+
+tabnine.setup({
+	max_lines                = 1000,
+	max_num_results          = 20,
+	sort                     = true,
+	run_on_every_keystroke   = true,
+	snippet_placeholder      = "..",
+	show_prediction_strength = false,
+})
 
 vim.opt.completeopt = "menuone,noselect"
 
-require("luasnip.loaders.from_lua").lazy_load({      paths = {"~/.config/nvim/bin/luasnip_snippets"}})
-require("luasnip.loaders.from_vscode").lazy_load({   paths = {"~/.config/nvim/bin/friendly-snippets"}})
-require("luasnip.loaders.from_snipmate").lazy_load({ paths = {"~/.config/nvim/bin/snippets"}})
+require("luasnip.loaders.from_lua").lazy_load({      paths = { "~/.config/nvim/bin/luasnip_snippets" } })
+require("luasnip.loaders.from_vscode").lazy_load({   paths = { "~/.config/nvim/bin/friendly-snippets" } })
+require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "~/.config/nvim/bin/snippets" } })
 
 local check_backspace = function()
-    local col = vim.fn.col(".") - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
---   פּ ﯟ   some other good icons
-local kind_icons = {
-    Copilot       = "",
-    Text          = "",
-    Method        = "",
-    Function      = "",
-    Constructor   = "",
-    Field         = "ﰠ",
-    Variable      = "",
-    Class         = "ﴯ",
-    Interface     = "",
-    Module        = "",
-    Property      = "ﰠ",
-    Unit          = "塞",
-    Value         = "",
-    Enum          = "",
-    Keyword       = "",
-    Snippet       = "",
-    Color         = "",
-    File          = "",
-    Reference     = "",
-    Folder        = "",
-    EnumMember    = "",
-    Constant      = "",
-    Struct        = "פּ",
-    Event         = "",
-    Operator      = "",
-    TypeParameter = "",
-    Table         = " ",
-    Object        = "",
-    Tag           = " ",
-    Array         = " ",
-    Boolean       = "蘒",
-    Number        = "",
-    String        = "",
-    Calendar      = " ",
-    Watch         = "",
-}
--- find more here: https://www.nerdfonts.com/cheat-sheet
+local has_any_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+		return false
+	end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local press = function(key)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
+end
+
+local t = function(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
 
 local SETTINGS = {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        end,
-    },
-    mapping = {
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-e>"] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        -- Accept currently selected item. If none selected, `select` first item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expandable() then
-                luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif check_backspace() then
-                fallback()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    },
-    formatting = {
-        fields = { "abbr", "kind", "menu" },
-        format = function(_, vim_item)
-            vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-            return vim_item
-        end,
-    },
-    sources = {
-        { name = "luasnip"      , group_index = 2 } ,
-        { name = "copilot"      , group_index = 2 } ,
-        { name = "nvim_lsp"     , group_index = 2 } ,
-        { name = "cmp_tabnine"  , group_index = 2 } ,
-        { name = "path"         , group_index = 2 } ,
-        { name = "buffer"       , group_index = 2 } ,
-        { name = "neosnippet" } ,
-    },
-    confirm_opts = { behavior = cmp.ConfirmBehavior.Replace, select = false },
-    window       = {
-        documentation = cmp.config.window.bordered(),
-        completion    = cmp.config.window.bordered(),
-    },
-    experimental = { ghost_text = false, native_menu = false },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	mapping = {
+		["<C-y>"] = cmp.config.disable,
+		["<C-e>"] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expandable() then
+				luasnip.expand()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif check_backspace() then
+				fallback()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		format = require("plug.lspkind").cmp_format({
+			with_text = true,
+			menu = {
+				nvim_lsp         = "[LSP]",
+				luasnip          = "[LuaSnip]",
+				buffer           = "[Buffer]",
+				nvim_lua         = "[Lua]",
+				ultisnips        = "[UltiSnips]",
+				vsnip            = "[vSnip]",
+				treesitter       = "[treesitter]",
+				look             = "[Look]",
+				copilot          = "[Copilot]",
+				path             = "[Path]",
+				spell            = "[Spell]",
+				calc             = "[Calc]",
+				emoji            = "[Emoji]",
+				neorg            = "[Neorg]",
+				cmp_openai_codex = "[Codex]",
+				cmp_tabnine      = "[TabNine]",
+			},
+		}),
+	},
+	sources = {
+		{ name = "luasnip", group_index = 2 },
+		{
+			name = "copilot",
+			group_index = 2,
+			trigger_characters = {
+				".",
+				":",
+				"(",
+				"'",
+				'"',
+				"[",
+				",",
+				"#",
+				"*",
+				"@",
+				"|",
+				"=",
+				"-",
+				"{",
+				"/",
+				"\\",
+				"+",
+				"?",
+				" ",
+			},
+		},
+		{ name = "nvim_lsp", group_index = 2 },
+		{ name = "cmp_tabnine", group_index = 2 },
+		{ name = "path", group_index = 2 },
+		{ name = "buffer", group_index = 2 },
+		{ name = "neosnippet" },
+	},
+	confirm_opts = { behavior = cmp.ConfirmBehavior.Replace, select = false },
+	window = {
+		documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+	},
+	experimental = { ghost_text = false },
+	-- view         = { entries = 'native' }
 }
 
 cmp.setup(SETTINGS)
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(":", {
+	sources = cmp.config.sources({
+		{ name = "path", max_item_count = 5 },
+	}, {
+		{ name = "cmdline", max_item_count = 15 },
+	}),
+})
+
+-- lsp_document_symbols
+cmp.setup.cmdline("/", {
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp_document_symbol", max_item_count = 8, keyword_length = 3 },
+	}, {
+		{ name = "buffer", max_item_count = 5, keyword_length = 5 },
+	}),
+})
+
+-- Database completion
+vim.api.nvim_exec(
+	[[
+        " autocmd! FileType sql setlocal omnifunc=vim_dadbod_completion#omni
+        autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+    ]],
+	false
+)
---
+++ lua/plug/lspkind.lua
@@ -0,0 +1,198 @@
+local lspkind = {}
+local fmt = string.format
+
+local kind_presets = {
+    default = {
+        Copilot       = "",
+        Tabnine       = "",
+        Class         = "",
+        Text          = "",
+        Method        = "",
+        Function      = "",
+        Constructor   = "",
+        Field         = "ﰠ",
+        Variable      = "",
+        Interface     = "",
+        Module        = "",
+        Property      = "ﰠ",
+        Unit          = "塞",
+        Value         = "",
+        Enum          = "",
+        Keyword       = "",
+        Snippet       = "",
+        Color         = "",
+        File          = "",
+        Reference     = "",
+        Folder        = "",
+        EnumMember    = "",
+        Constant      = "",
+        Struct        = "פּ",
+        Event         = "",
+        Operator      = "",
+        TypeParameter = ""
+    },
+    codicons = {
+        Copilot       = "",
+        Tabnine       = "",
+        Text          = "",
+        Method        = "",
+        Function      = "",
+        Constructor   = "",
+        Field         = "",
+        Variable      = "",
+        Class         = "",
+        Interface     = "",
+        Module        = "",
+        Property      = "",
+        Unit          = "",
+        Value         = "",
+        Enum          = "",
+        Keyword       = "",
+        Snippet       = "",
+        Color         = "",
+        File          = "",
+        Reference     = "",
+        Folder        = "",
+        EnumMember    = "",
+        Constant      = "",
+        Struct        = "",
+        Event         = "",
+        Operator      = "",
+        TypeParameter = ""
+    }
+}
+
+local kind_order = {
+    "Copilot", "Text", "Method", "Function", "Constructor", "Field", "Variable",
+    "Class", "Interface", "Module", "Property", "Unit", "Value", "Enum",
+    "Keyword", "Snippet", "Color", "File", "Reference", "Folder", "EnumMember",
+    "Constant", "Struct", "Event", "Operator", "TypeParameter"
+}
+local kind_len = 25
+
+local function get_symbol(kind)
+    local symbol = lspkind.symbol_map[kind]
+    if kind == "copilot" then symbol = lspkind.symbol_map["Copilot"] end
+    return symbol or ""
+end
+
+local modes = {
+    ["text"] = function(kind) return kind end,
+    ["text_symbol"] = function(kind)
+        local symbol = get_symbol(kind)
+        return fmt("%s %s", kind, symbol)
+    end,
+    ["symbol_text"] = function(kind)
+        local symbol = get_symbol(kind)
+        return fmt("%s %s", symbol, kind)
+    end,
+    ["symbol"] = function(kind)
+        local symbol = get_symbol(kind)
+        return fmt("%s", symbol)
+    end
+}
+
+-- default true
+-- deprecated
+local function opt_with_text(opts)
+    return opts == nil or opts["with_text"] == nil or opts["with_text"]
+end
+
+-- default 'symbol'
+local function opt_mode(opts)
+    local mode = "symbol"
+    if opt_with_text(opts) and opts ~= nil and opts["mode"] == nil then
+        mode = "symbol_text"
+    elseif opts ~= nil and opts["mode"] ~= nil then
+        mode = opts["mode"]
+    end
+    return mode
+end
+
+-- default 'default'
+local function opt_preset(opts)
+    local preset
+    if opts == nil or opts["preset"] == nil then
+        preset = "default"
+    else
+        preset = opts["preset"]
+    end
+    return preset
+end
+
+function lspkind.init(opts)
+    if opts ~= nil and opts["with_text"] ~= nil then
+        vim.api.nvim_command("echoerr 'DEPRECATED replaced by mode option.'")
+    end
+    local preset = opt_preset(opts)
+
+    local symbol_map = kind_presets[preset]
+    lspkind.symbol_map = (opts and opts["symbol_map"] and
+                             vim.tbl_extend("force", symbol_map,
+                                            opts["symbol_map"])) or symbol_map
+
+    local symbols = {}
+    local len = kind_len
+    for i = 1, len do
+        local name = kind_order[i]
+        symbols[i] = lspkind.symbolic(name, opts)
+    end
+
+    for k, v in pairs(symbols) do
+        require("vim.lsp.protocol").CompletionItemKind[k] = v
+    end
+end
+
+lspkind.presets = kind_presets
+lspkind.symbol_map = kind_presets.codicons
+
+function lspkind.symbolic(kind, opts)
+    local mode = opt_mode(opts)
+    local formatter = modes[mode]
+
+    -- if someone enters an invalid mode, default to symbol
+    if formatter == nil then formatter = modes["symbol"] end
+
+    return formatter(kind)
+end
+
+function lspkind.cmp_format(opts)
+    if opts == nil then opts = {} end
+    if opts.preset or opts.symbol_map then lspkind.init(opts) end
+
+    return function(entry, vim_item)
+        if opts.before then vim_item = opts.before(entry, vim_item) end
+
+        vim_item.kind = lspkind.symbolic(vim_item.kind, opts)
+
+        if opts.menu ~= nil then
+            vim_item.menu = opts.menu[entry.source.name]
+        end
+
+        if opts.maxwidth ~= nil then
+            if opts.ellipsis_char == nil then
+                vim_item.abbr = string.sub(vim_item.abbr, 1, opts.maxwidth)
+            else
+                local label = vim_item.abbr
+                local truncated_label = vim.fn.strcharpart(label, 0,
+                                                           opts.maxwidth)
+                if truncated_label ~= label then
+                    vim_item.abbr = truncated_label .. opts.ellipsis_char
+                end
+            end
+        end
+
+        -- Copilot Item Check
+        if entry.source.name == "copilot" then
+            vim_item.kind = string.format("%s %s", "", "Copilot")
+        end
+
+        -- Tabnine Item Check
+        if entry.source.name == "cmp_tabnine" then
+            vim_item.kind = string.format("%s %s", "", "Tabnine")
+        end
+        return vim_item
+    end
+end
+
+return lspkind

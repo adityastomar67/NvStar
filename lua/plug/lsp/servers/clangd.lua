c28c02a27a386319b415f1337062b6688e319c8a
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-10-20 12:08:40 +0530

    server settings

--- lua/plug/lsp/servers/clangd.lua
+++ lua/plug/lsp/servers/clangd.lua
@@ -2,34 +2,34 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
 
 local SETTINGS = {
-    capabilities = capabilities,
-    cmd = {
-      "clangd",
-      "--background-index",
-      "--pch-storage=memory",
-      "--clang-tidy",
-      "--suggest-missing-includes",
-      "--cross-file-rename",
-      "--completion-style=detailed",
-    },
-    init_options = {
-      clangdFileStatus = true,
-      usePlaceholders = true,
-      completeUnimported = true,
-      semanticHighlighting = true,
-    },
-    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
-    log_level = 2,
-    root_dir = require("lspconfig.util").root_pattern({
-      ".clangd",
-      ".clang-tidy",
-      ".clang-format",
-      "compile_commands.json",
-      "compile_flags.txt",
-      "configure.ac",
-      ".git",
-    }) or vim.loop.cwd(),
-    single_file_support = true,
-  }
-  
-  return SETTINGS
+	capabilities = capabilities,
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--pch-storage=memory",
+		"--clang-tidy",
+		"--suggest-missing-includes",
+		"--cross-file-rename",
+		"--completion-style=detailed",
+	},
+	init_options = {
+		clangdFileStatus     = true,
+		usePlaceholders      = true,
+		completeUnimported   = true,
+		semanticHighlighting = true,
+	},
+	filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+	log_level = 2,
+	root_dir = require("lspconfig.util").root_pattern({
+		".clangd",
+		".clang-tidy",
+		".clang-format",
+		"compile_commands.json",
+		"compile_flags.txt",
+		"configure.ac",
+		".git",
+	}) or vim.loop.cwd(),
+	single_file_support = true,
+}
+
+return SETTINGS
--- lua/plug/lsp/servers/emmet-ls.lua
+++ lua/plug/lsp/servers/emmet-ls.lua
@@ -1,13 +1,28 @@
 local SETTINGS = {
-    default_config = {
-        cmd = {"ls_emmet", "--stdio"},
-        filetypes = {
-            "html", "css", "scss", "javascript", "javascriptreact",
-            "typescript", "typescriptreact", "haml", "xml", "xsl", "pug",
-            "slim", "sass", "stylus", "less", "sss"
-        },
-        root_dir = function(fname) return vim.loop.cwd() end
-    }
+	default_config = {
+		cmd = { "ls_emmet", "--stdio" },
+		filetypes = {
+			"html",
+			"css",
+			"scss",
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"haml",
+			"xml",
+			"xsl",
+			"pug",
+			"slim",
+			"sass",
+			"stylus",
+			"less",
+			"sss",
+		},
+		root_dir = function(fname)
+			return vim.loop.cwd()
+		end,
+	},
 }
 
 return SETTINGS

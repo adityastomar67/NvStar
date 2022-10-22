6b1991d9eab9b578544ccedcfa887103e6ddb427
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-10-22 08:12:10 +0530

    loaded plugins disabled

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -162,7 +162,7 @@ local disabled_built_ins = {
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-    vim.g["loaded_" .. plugin] = 1
+    vim.g["loaded_" .. plugin] = 0
 end
 
 local default_providers = {

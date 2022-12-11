2ea23695a80567a0e47233444b59dba0572f9ec4
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-12-11 09:37:38 +0530

    Added more dynamic options

--- lua/plug/cmp/dynamic.lua
+++ lua/plug/cmp/dynamic.lua
@@ -25,6 +25,60 @@ local SETTINGS = {
             end,
         },
     },
+    {
+        label = "next Tuesday",
+        insertText = 1,
+        cb = {
+            function()
+                return Date.new():add_date(7):day(2):format("%Y/%m/%d")
+            end,
+        },
+    },
+    {
+        label = "next Wednesday",
+        insertText = 1,
+        cb = {
+            function()
+                return Date.new():add_date(7):day(3):format("%Y/%m/%d")
+            end,
+        },
+    },
+    {
+        label = "next Thursday",
+        insertText = 1,
+        cb = {
+            function()
+                return Date.new():add_date(7):day(4):format("%Y/%m/%d")
+            end,
+        },
+    },
+    {
+        label = "next Friday",
+        insertText = 1,
+        cb = {
+            function()
+                return Date.new():add_date(7):day(5):format("%Y/%m/%d")
+            end,
+    },
+    {
+        label = "next Saturday",
+        insertText = 1,
+        cb = {
+            function()
+                return Date.new():add_date(7):day(6):format("%Y/%m/%d")
+            end,
+        },
+    },
+    {
+        label = "next Sunday",
+        insertText = 1,
+        cb = {
+            function()
+                return Date.new():add_date(7):day(7):format("%Y/%m/%d")
+            end,
+        },
+    },
+    }
 }
 
 dynamic.register(SETTINGS)
